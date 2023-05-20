bee76baa9a810ea14733937f84a41cff31599f8e
AnthonyBertottiFavero <bertottianthony1@gmail.com>
2023-05-20 15:56:56 +0200

    add commit

---
+++ script/script.js
@@ -0,0 +1,64 @@
+/*
+    Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+    Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+    Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+    Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+    Scott Estrada	Developer	scott-estrada-developer.jpg
+    Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+*/
+
+
+
+const teamMembers = [
+    {
+        nameSurname: 'Wayne Barnett', 
+        job: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        nameSurname: 'Angela Caroll', 
+        job: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },  
+    
+    {
+        nameSurname: 'Walter Gordon', 
+        job: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        nameSurname: 'Angela Lopez', 
+        job: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+
+
+    {
+        nameSurname: 'Scott Estrada', 
+        job: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        nameSurname: 'Barbara Ramos', 
+        job: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+let cardContainer = document.querySelector('.cards');
+
+for ( let i = 0; i < teamMembers.length; i++) {
+    let members = teamMembers[i];
+    console.log(`${i}. Nome e Cognome: ${members.nameSurname}, Incarico: ${members.job}, Immagine: ${members.image}.`);
+
+    cardContainer.innerHTML += `<div class="card">
+    <img src="img/${members.image}">
+
+    <h3 class="name-surname">${members.nameSurname}</h3>
+
+    <h5 class="job">${members.job}</h6>
+</div>`
+};
